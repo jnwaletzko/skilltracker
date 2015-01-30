0acbd21b7a3c2c03bb911ba1c83ee55a52a0769d
jnwaletzko <jnwaletzko@gmail.com>
2015-01-30 16:59:36 -0600

    Ability to create skills

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,8 +1,20 @@
 class SkillsController < ApplicationController
+  def show
+    @skill = Skill.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:skill].inspect
+    @skill = Skill.new(skill_params)
+
+    @skill.save
+    redirect_to @skill
   end
+
+  private
+    def skill_params
+      params.require(:skill).permit(:title, :description)
+    end
 end
