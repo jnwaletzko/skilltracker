804d4babaabc3878048a6ea9b64939f728a93370
jnwaletzko <jnwaletzko@gmail.com>
2015-02-02 13:21:46 -0600

    Connected pages

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,16 +1,24 @@
 class SkillsController < ApplicationController
+  def index
+    @skills = Skill.all
+  end
+
   def show
     @skill = Skill.find(params[:id])
   end
 
   def new
+    @skill = Skill.new
   end
 
   def create
     @skill = Skill.new(skill_params)
 
-    @skill.save
-    redirect_to @skill
+    if @skill.save
+      redirect_to @skill
+    else
+      render 'new'
+    end
   end
 
   private
