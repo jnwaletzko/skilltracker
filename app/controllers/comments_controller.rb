69a6cf76e301408c53d202012e987acbfd9602d6
jnwaletzko <jnwaletzko@gmail.com>
2015-02-04 16:14:35 -0600

    Add functionality to delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
     redirect_to skill_path(@skill)
   end
 
+  def destroy
+    @skill = Skill.find(params[:skill_id])
+    @comment = @skill.comments.find(params[:id])
+    @comment.destroy
+    redirect_to skill_path(@skill)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
