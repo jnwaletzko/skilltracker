dac80d3b4bd9c47fa84ae5dc3506024e08844f9d
jnwaletzko <jnwaletzko@gmail.com>
2015-02-04 16:27:21 -0600

    Add security for skills and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+
   def create
     @skill = Skill.find(params[:skill_id])
     @comment = @skill.comments.create(comment_params)
--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,4 +1,7 @@
 class SkillsController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @skills = Skill.all
   end
